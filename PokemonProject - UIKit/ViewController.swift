5d87334ed14e5203e8970eecf14192dcddb21207
Leonardo <leonardo.fleith@MacBook-Air-de-Leonardo.local>
2024-05-08 22:19:51 -0300

    first commit

---
+++ PokemonProject - UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonProject - UIKit
+//
+//  Created by Leonardo on 08/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
